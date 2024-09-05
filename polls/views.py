786f7f75c258992e813b7889bd4a32479ef12827
swapno963 <swapno963@gmail.com>
2024-09-05 17:03:29 +0600

    Various types of test written

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,8 @@ from django.http import Http404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
+
 
 
 def index(request):
@@ -68,7 +70,22 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+    
+    
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
